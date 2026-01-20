4a5fe7711d84fa28ae605db1476150b4ce4603bf
Prasham-Karkera <prashamkarkera@gmail.com>
2026-01-20 20:17:50 +0530

    cleanup :)

--- backend_doc/gg.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAja0uTquZyQrZEr3Q5W7zDhSvQuZ5wE7I")
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
